63879ac7a13d53262217a9838581932446faaa84
nghkh <107782283+nghkh@users.noreply.github.com>
2022-06-19 12:54:04 +0700

    Add files via upload

--- BT1.dart
+++ BT1.dart
@@ -1,14 +1,12 @@
 void main(List<String> args) {
-  // print('Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (phần 1)'); //cách 1
-  
-  // String s = 'Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (phần 1)'; // cách 2
-  // print(s);
-
-  // bai2(); // cách 3
-
-
+    String str = " đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)…";
+    List<String> cutstr = str.split(" ");
+  String s1 = '${cutstr[1].toString().substring(0,1).toUpperCase()}${cutstr[1].toString().substring(1,3)}';
+  String s2 = '${cutstr[2].toString()} ${cutstr[3].toString()}';
+  String s3 = '${cutstr[4].toString()} ${cutstr[5].toString()}';
+  String s4 = '${cutstr[6].toString()} ${cutstr[7].toString()}';
+  String s5 = '${cutstr[8].toString()} ${cutstr[9].toString()}';
+  String s6 = '${cutstr[10].toString()} ${cutstr[11].toString().substring(0,1).toUpperCase()}${cutstr[11].toString().substring(1,5)}';
+  String s7 = '${cutstr[12].toString().toUpperCase()} ${cutstr[13].toString().toUpperCase()} ${cutstr[14].toString()} ${cutstr[15].toString().substring(0,2)}';
+    print('\"$s1 $s2 $s3 $s4 $s5 $s6 $s7\"');
 }
-
-// void bai2() { // cách 3
-//   print('Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (phần 1)');
-// }
